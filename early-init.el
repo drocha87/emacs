e289ca3e75e60916374524ad42037a017e33e070
Diego Rocha <dlmrocha87@gmail.com>
2023-01-16 19:19:00 -0300

    remove straight package manager

--- early-init.el
+++ early-init.el
@@ -1,1 +1,28 @@
-(setq package-enable-at-startup nil)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(dolist (var '(default-frame-alist initial-frame-alist))
+  (add-to-list var '(width . (text-pixels . 1200)))
+  (add-to-list var '(height . (text-pixels . 900))))
+
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-resize t)
+
+(setq use-dialog-box t ; only for mouse events, which I seldom use
+      use-file-dialog nil
+      inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-startup-echo-area-message user-login-name ; read the docstring
+      inhibit-startup-buffer-menu t)
+
+;; Initialise installed packages
+(setq package-enable-at-startup t)
+
+(defvar package-quickstart)
+
+;; Allow loading from the package cache
+(setq package-quickstart t)
+
+(setq native-comp-async-report-warnings-errors 'silent) ; emacs28 with native compilation
+
--- init.el
+++ init.el
@@ -1,72 +1,84 @@
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
+;; Some basic settings
+(setq frame-title-format '("%b"))
+(setq ring-bell-function 'ignore)
+(setq use-short-answers t)
+(setq initial-buffer-choice t) ; always start with *scratch*
+(delete-selection-mode 1)
 
 (load-theme 'modus-operandi t)
-(global-display-line-numbers-mode 1)
-(global-hl-line-mode 1)
-(delete-selection-mode 1)
+
+(require 'package)
+;; All variables are for Emacs 28+
+(setq package-name-column-width 40)
+(setq package-version-column-width 14)
+(setq package-status-column-width 12)
+(setq package-archive-column-width 8)
+(add-hook 'package-menu-mode-hook #'hl-line-mode)
+
+;; Also read: <https://protesilaos.com/codelog/2022-05-13-emacs-elpa-devel/>
+(setq package-archives
+      '(("elpa" . "https://elpa.gnu.org/packages/")
+        ("elpa-devel" . "https://elpa.gnu.org/devel/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+        ("melpa" . "https://melpa.org/packages/")))
+
+;; Highest number gets priority (what is not mentioned has priority 0)
+(setq package-archive-priorities
+      '(("elpa" . 2)
+        ("nongnu" . 1)))
+
+;; (global-display-line-numbers-mode 1)
+;; (global-hl-line-mode 1)
 
 (setq backup-directory-alist '(("" . "~/.config/emacs/backups")))
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(use-package which-key :ensure t :init (which-key-mode 1))
+(use-package ivy :ensure t :init (ivy-mode 1))
+(use-package expand-region :ensure t :config (global-set-key (kbd "C-=") 'er/expand-region))
+(use-package editorconfig :ensure t :init (editorconfig-mode 1))
+(use-package ansi-color :ensure t :hook (compilation-filter . ansi-color-compilation-filter))
+(use-package savehist :init (savehist-mode))
+(use-package magit :ensure t)
+(use-package move-text :ensure t :init (move-text-default-bindings))
 
-(straight-use-package 'use-package)
+;; Whether to save existing clipboard text into kill ring before replacing it.
+;; (setq save-interprogram-paste-before-kill t)
 
-(use-package which-key :straight t :init (which-key-mode 1))
-(use-package ivy :straight t :init (ivy-mode 1))
-(use-package expand-region :straight t :config (global-set-key (kbd "C-=") 'er/expand-region))
-(use-package editorconfig :straight t :init (editorconfig-mode 1))
-(use-package ansi-color :straight t :hook (compilation-filter . ansi-color-compilation-filter))
+;; (use-package vertico
+;;   :straight t
+;;   :init
+;;   (vertico-mode)
+;;   ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+;;   (setq vertico-cycle t))
 
-(use-package vertico
-  :straight t
-  :init
-  (vertico-mode)
-  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
-  (setq vertico-cycle t))
+;; (use-package orderless
+;;   :straight t
+;;   :init
+;;   (setq completion-styles '(orderless basic)
+;;         completion-category-defaults nil
+;;         completion-category-overrides '((file (styles partial-completion)))))
 
-(use-package orderless
-  :straight t
-  :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
-
-;; Enable rich annotations using the Marginalia package
-(use-package marginalia
-  :straight t
-  ;; Either bind `marginalia-cycle' globally or only in the minibuffer
-  :bind (("M-A" . marginalia-cycle)
-         :map minibuffer-local-map
-         ("M-A" . marginalia-cycle))
-  :init (marginalia-mode))
-
-;; Persist history over Emacs restarts. Vertico sorts by history position.
-(use-package savehist
-  :init
-  (savehist-mode))
-
-(use-package magit :straight t)
-
-(use-package move-text :straight t :init (move-text-default-bindings))
+;; ;; Enable rich annotations using the Marginalia package
+;; (use-package marginalia
+;;   :straight t
+;;   ;; Either bind `marginalia-cycle' globally or only in the minibuffer
+;;   :bind (("M-A" . marginalia-cycle)
+;;          :map minibuffer-local-map
+;;          ("M-A" . marginalia-cycle))
+;;   :init (marginalia-mode))
 
 (global-set-key [f5] #'compile)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(move-text magit ansi-color which-key use-package ivy expand-region editorconfig)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
